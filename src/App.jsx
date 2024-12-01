de7017a6e62684c83af025c0183c48a301cd90a7
Saifulbabu50 <srmdsaifulsaif@gmail.com>
2024-12-02 04:13:42 +0600

    first country

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Countries from './componnets/Countries/Countries'
+
+function App() {
+  
+
+  return (
+    <>
+    
+      <h1>React- world -Tour</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/componnets/Country/Country.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './Country.css'
+const Country = ({ country }) => {
+    console.log(country)
+    const { name, flags, population, area, capital,
+        maps, cca3 } = country;
+
+    const [visited, setVisited] = useState(false);
+    
+    const handleVisited =() =>{
+        setVisited(!visited)
+    }
+    return (
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+            <h3 style={{ color: visited ? 'Purple': 'white'}}>{name.common}</h3>
+            <img src={flags.png} alt="" />
+            <p>population:{population}</p>
+            <p>Area:{area}</p>
+            <p>maps:{maps?.googleMaps}</p>
+            <p>capital:{capital}</p>
+            <p><small>code:{cca3}</small></p>
+            <button >Mark visited</button>
+            <br />
+            <button onClick={handleVisited}>{visited ? 'Visited': 'Going' }</button>
+            {visited ? 'I have visited this country.' :'I want to visited'}
+        </div>
+    );
+};
+
+export default Country;
