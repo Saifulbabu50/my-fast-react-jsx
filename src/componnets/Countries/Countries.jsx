495649af0142ff51700f2572245222057c61e192
Saifulbabu50 <srmdsaifulsaif@gmail.com>
2024-12-02 22:29:33 +0600

    first setallcountries

--- src/componnets/Countries/Countries.jsx
+++ src/componnets/Countries/Countries.jsx
@@ -5,7 +5,7 @@ import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
-
+    const [visitedCountries, setVisitrdCountries]= useState([])
    
 
     useEffect(() => {
@@ -14,23 +14,31 @@ const Countries = () => {
             .then(data => setCountries(data))
     }, [])
 
-    
+    const handleVisitedCountry = country =>{
+        console.log('add to this visitrd country')
+        const newVisitedCountries = [...visitedCountries, country]
+        setVisitrdCountries(newVisitedCountries)
+    }
     
 
     return (
         <div>
             <h3>Countries{countries.length}</h3>
             <div>
-                <h5>Visited countries: </h5>
+                <h5>Visited countries:{visitedCountries.length} </h5>
                 <ul>
-
+                    {
+                        visitedCountries.map(country => <li key={country .cca3}>
+                            {country.name.common}
+                        </li>)  
+                    }
                 </ul>
             </div>
             <div className="Coumtries-continer">
                 {
                     countries.map(country => <Country
                         key={country.cca3}
-                       
+                        handleVisitedCountry={handleVisitedCountry}
                         country={country}></Country>)
                 }
             </div>
